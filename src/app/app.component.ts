2812435ea6c53acd774ae3bc892bad4fe16db172
AbdallahMohamed79 <113598906+AbdallahMohamed79@users.noreply.github.com>
2024-02-01 14:02:13 +0200

    added the login component and edit some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent],
+  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ 
+    {
+    path: "",
+    component: HomeComponent, title:"Home"
+  },
+  {
+    path: "home",
+    component: HomeComponent, title:"Home"
+  },
+     {
+    path: "login",
+    component: LoginComponent, title:"login",
+  },
+];
