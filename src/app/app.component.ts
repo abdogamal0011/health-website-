f335cd2af30e5ab6a27f014fa0c89782140b3400
“Ahmed <ahmess4666@gmail.com>
2024-02-01 17:14:40 +0200

    I Have Added Register Page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent],
+  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent , RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: "",
+        component: HomeComponent ,
+        title: "  Home Page "
+    },
+
+    {
+        path: "Register",
+        component: RegisterComponent ,
+        title: " Registeration Page "
+    },
+];
