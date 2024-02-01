58e96fc8e10e43b687d58c73b07eb2a7afa8e914
AbdallahMohamed79 <113598906+AbdallahMohamed79@users.noreply.github.com>
2024-02-01 18:07:29 +0200

    added register component and some changes for it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,12 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { LoginComponent } from './components/login/login.component';
-
+import { RegisterComponent } from './components/register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent, LoginComponent],
+  imports: [RouterOutlet , NavbarComponent , HomeComponent,FooterComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
  
@@ -15,5 +16,9 @@ export const routes: Routes = [
      {
     path: "login",
     component: LoginComponent, title:"login",
+  },
+     {
+    path: "register",
+    component: RegisterComponent, title:"register",
   },
 ];
