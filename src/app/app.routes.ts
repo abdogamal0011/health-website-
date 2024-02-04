13355b9a41a089f3c91bfe6940274e4f84b11b2b
Ahmed Maky <129126532+ahmedmansour999@users.noreply.github.com>
2024-02-04 07:44:19 +0200

    mans

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DoctorCardComponent } from './doctor-card/doctor-card.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home" , component: HomeComponent , title:"Home"} ,
+
+
+
+  {path:"" , redirectTo:"home" , pathMatch:"full"}
+];
