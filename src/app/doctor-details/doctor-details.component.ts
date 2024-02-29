607a072e0df28985082f9708cb654e6d42e460ef
Ahmed Maky <129126532+ahmedmansour999@users.noreply.github.com>
2024-02-29 12:44:02 +0200

    api

--- src/app/doctor-details/doctor-details.component.ts
+++ src/app/doctor-details/doctor-details.component.ts
@@ -1,28 +1,72 @@
 import { ListDoctorService } from './../service/list-doctor.service';
 import { Component } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ListDoctor } from '../interface/list-doctor';
+import { FormsModule } from '@angular/forms'; // Import FormsModule
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-doctor-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './doctor-details.component.html',
   styleUrl: './doctor-details.component.css',
 })
 export class DoctorDetailsComponent {
   doctor: any;
+  api : any ;
+
+  // this data to test function work
+  data :any = {
+    'name' : "tamer" ,
+    "rate" : '50'
+}
+
 
   constructor(
     private activatedRoute: ActivatedRoute,
-    private listDoctorService: ListDoctorService
+    private listDoctorService: ListDoctorService ,
+    private apiService:ApiService
   ) {}
 
   ngOnInit(): void {
     const id = this.activatedRoute.snapshot.params['id'];
     this.listDoctorService.DoctorDetails(id).subscribe((item : any) => {
-      this.doctor = item.data;
+      this.doctor = item.data
       console.log(this.doctor);
+
     });
+
+    this.apiService.getData().subscribe(data => {
+       this.api = data
+      console.log(this.api);
+
+    }) ;
+
+
+    this.apiService.postData(this.data).subscribe(item => {
+      this.api = item
+      console.log("post " , this.api);
+
+    })
+
+
+
   }
+
+
+
+
+
+  test(){
+    this.apiService.patchData(1 , this.data).subscribe(item => {
+      this.api = item
+      console.log("post " , this.api);
+
+    })
+  }
+
+
+
 }
+
+
